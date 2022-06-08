2e36a98a238154906afb7a6c2a95ee2003cf14b3
eunyeoul <eunyull102@nate.com>
2022-06-09 00:51:14 +0900

    동전문제

--- baekjoon_greedy.py
+++ baekjoon_greedy.py
@@ -27,28 +27,34 @@ def Answer():
         result += 1
 
 
+def CoinAnswer():
+    n = 10
+    k = 4200
+    inputCoin = 0
+    divCount = 0
+    moc = 0
+    namuge = 0
+
+    # list = [1, 5, 10, 50, 100, 1000, 5000, 10000, 50000]
+    inputList = []
+    if inputCoin <k:
+        heapq.heappush(inputList,inputCoin)
+
+
+
+    coin = 1000
+    while k != 0:
+        moc = k // coin
+        namuge = k % coin
+        k = namuge
+        divCount = moc
+
+# 힙에서 가장큰 수를 뽑아서 나누다가 나머지가 0이 되면 바복문 끝남
+# 궁금한점은 heap pop에서 list가 0일때 어떻게 되는지??
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    n = int(input())
-    data = list(map(int, input().split()))
-    eachSum = n * [0]
-    data.sort()
-
-#     정렬을 한다
-#  1 , 1+2 , 1+2+3 , 1+2+3+3 , 1+2+3+3+4 ,
-# f(0) = list[0]
-# f(1) = f(0)+list[1]
-# f(2) = f(1) + list[2]
-
-#  f0 부터 f 4까지 더한다
-
-    for i in range(0, n):
-        if i == 0:
-            eachSum[0] = data[0]
-        else:
-            eachSum[i] = eachSum[i - 1] + data[i]
-
-    print(sum(eachSum))
+    CoinAnswer()
 
 #  이 모든 값을 더한다.
 
