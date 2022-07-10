cfd6306d8fbc46944d574e05e4fbed76c5b6cc17
youeuneyol <ey.yoo@fanmaum.com>
2022-07-10 22:27:14 +0900

    이것이 코테다

--- this_is_coding_test_for_apply.py
+++ this_is_coding_test_for_apply.py
@@ -34,4 +34,30 @@ def 체크함수4_1(n, position):
 
 
 if __name__ == '__main__':
-    이것이_취업을위한_코테다_예제_4_1_상하좌우()
+    print("asdf")
+    # 이것이_취업을위한_코테다_예제_4_1_상하좌우()
+
+    ## n,m을 공백으로 구분하여 입력받기
+
+    ## n,m = map(int ,input().split())
+
+    #2차원 배열 초기화
+    # n= 5
+    # m = 5
+    # d = [[0]*m for _ in range(n)]
+
+    # print(d)
+
+    #3개 입력받기
+    #x,y,direction = map(int,input().split())
+
+    #2차원 배열 입력 받기
+
+    # array = []
+    # n = 5
+    # for i in range(n):
+    #     array.append(list(map(int,input().split())))
+    #
+    # print(array)
+
+
